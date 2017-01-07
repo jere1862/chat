ee19ac74c476552615c69a478802cc97b9c54252
Jeremie Roy <j-roy95@hotmail.com>
2017-01-07 17:46:41 -0500

    Continue progress on user interface

--- public/javascripts/bubbles.js
+++ public/javascripts/bubbles.js
@@ -1,15 +1,29 @@
 var socket = io();
+var bubbles = [];
 $(document).ready(function(){
     socket.on('update', function(channelName){
         console.log(channelName);
+		addBubble(channelName);
     });
 
     $('#btn').click(function(){
-        $('#bubbleHolder').append('<a href=# class="circle"><p class="bubbleText">test</p></a>')
-        //socket.emit('update');
+        //$('#bubbleHolder').append('<a href=# class="circle"><p class="bubbleText">test</p></a>')
+        socket.emit('update');
     });
 
     $('#btn2').click(function(){
-        $('.circle:first').animate({"left": "+=50px"}, 'slow');
+        $('.circle:first').animate({"margin-left": "+=50px"}, 'slow');
     });
 });
+
+function bubble(x, y, radius){
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+}
+
+function addBubble(bubble){
+	var angle = Math.random()*360;
+	//bubbles.push(new bubble(0,0,0));
+	$('#bubbleHolder').append('<a href=# class="circle"><p class="bubbleText">test</p></a>');
+}
--- routes/index.js
+++ routes/index.js
@@ -6,30 +6,53 @@ var moniker = require('moniker');
 var people = {};
 var bubbles = {};
 
+function person(name, room){
+	this.name = name;
+	this.room = room;
+}
+
+function bubble(name){
+	this.users = [];
+	this.population = 1 ;
+}
+
 module.exports = function(io){
   io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-      io.sockets.in(msg.room).emit('chat message', people[socket.id]+': '+msg.message);
+      io.sockets.in(msg.room).emit('chat message', people[socket.id].name+': '+msg.message);
     });
 
     socket.on('room connection', function(channelName){
-      if(people[socket.id]==undefined){
+	  if(people[socket.id]==undefined){
         // Assign a random name
-        people[socket.id] = moniker.choose();
-      }
+        people[socket.id] = new person(moniker.choose(), channelName);
+      }else{
+		var oldRoom = people[socket.id].room;
+		bubbles[oldRoom].population--;
+		people[socket.id].room = channelName;
+	  }
       socket.join(channelName);
       socket.emit('room connection', channelName);
-      io.sockets.in(channelName).emit('chat message', people[socket.id]+' joined '+channelName+'.');
-      if(bubbles[channelName] == undefined){
-        bubbles[channelName] = 1;
+      io.sockets.in(channelName).emit('chat message', people[socket.id].name+' joined '+channelName+'.');
+      
+	  if(bubbles[channelName] == undefined){
+        bubbles[channelName] = new bubble(channelName);
       }else{
-        bubbles[channelName] += 1;
+        bubbles[channelName].population++;
       }
+	  bubbles[channelName].users.push(people[socket.id]);
     });
 
     socket.on('disconnect', function(){
-      console.log(people[socket.id]+' disconnected.');
-      delete people[socket.id];
+      if(people[socket.id] != undefined){
+		console.log(people[socket.id].name+' disconnected.');
+		var room = people[socket.id].room;
+		var index = bubbles.indexOf(room);
+		if(index != -1){
+			array.splice(index, 1);
+		}
+		delete people[socket.id];
+	  }
     });
 
    /* Bubble homepage */
@@ -38,9 +61,13 @@ module.exports = function(io){
     router.get('/', function(req, res, next) {
       res.render('home', { bubbles: bubbles });
     });
+	
+	router.get('/test', function(req, res, next) {
+	  res.render('index', {'title': 'test'})
+	});
 
     socket.on('update', function(){
-      socket.emit('update', 'testChannel');
+      socket.emit('update', {'bubbles': bubbles});
     });
 
   });
