49b637faacc271054bcb5eafc0ac27306b558a46
Jeremie Roy <j-roy95@hotmail.com>
2017-08-13 19:16:04 -0400

    * Make bubbles resize dynamically * Fix 404 on first load

--- public/javascripts/bubbles.js
+++ public/javascripts/bubbles.js
@@ -1,97 +1,92 @@
 $(document).ready(function(){
-    var svg = d3.select("svg"),
+
+    var socket = io();
+    const MAX_BUBBLE_RADIUS = 100;
+
+    var bubblesHierarchy = {
+        "name": "default",
+        "children": [
+        ]
+    }
+
+    window.onresize = function(){
+        var svg = d3.select("svg");
+        svg.remove();
+        drawSvg();
+    };
+
+    socket.emit("bubblesUpdate");
+
+    socket.on("bubblesUpdate", function(res){
+        var bubbles = [];
+        res.forEach(function(bubble){
+            bubbles.push({
+                "name": bubble.id,
+                "value": bubble.users.length
+            });
+        });
+        bubblesHierarchy.children = bubbles;
+        drawSvg()
+    });
+
+    function drawSvg(){
+        if(bubblesHierarchy.children.length==0) return;
+        var svg = d3.selectAll("body").append("svg")
+            .attr("width", window.innerWidth)
+            .attr("height", window.innerHeight)
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "10")
+            .attr("text-anchor", "middle");
+
         width = +svg.attr("width"),
         height = +svg.attr("height");
 
-    var pack = d3.pack()
-        .size([width, height])
-        .padding(1.5);
+        var pack = d3.pack()
+            .size([width, height]);
+        var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-    var format = d3.format(",d");
+        var root = d3.hierarchy(bubblesHierarchy)
+            .sum(function(d) { return d.value; })
+            .each(function(d) {
+                d.id = d.data.name;
+            });
 
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
+        var node = svg.selectAll(".node")
+            .data(pack(root).leaves())
+            .enter().append("a")
+                .attr("class", "node")
+                .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+                .attr("href", function(d){return d.id});
+        
+        node.append("circle")
+            .attr("id", function(d) { return d.name;})
+            .attr("r", function(d) {
+                 if(d.r > MAX_BUBBLE_RADIUS){
+                    return MAX_BUBBLE_RADIUS;
+                 }
+                 return d.r;
+             })
+            .style("fill", function(d) { return color(d.value); })
+            .style("stroke", "black")
+            .style("stroke-width", "2px")
 
-    var hierarchy = {
-        "name": "Eve",
-        "value": 12,
-        "children": [
-            {
-            "name": "Cain",
-            "value": 20,
-            },
-            {
-            "name": "Seth",
-            "value": 100,
-            "children": [
-                {
-                "name": "Enos",
-                "value": 15
-                },
-                {
-                "name": "Noam",
-                "value": 150
-                }
-            ]
-            },
-            {
-            "name": "Abel",
-            "value": 4
-            },
-            {
-            "name": "Awan",
-            "value": 24,
-            "children": [
-                {
-                "name": "Enoch",
-                "value": 4
-                }
-            ]
-            },
-            {
-            "name": "Azura",
-            "value": 16
-            }
-        ]
-    };
+        node.append("clipPath")
+            .attr("id", function(d) { return "clip-" + d.id; })
+            .append("use")
+            .attr("xlink:href", function(d) { return "#" + d.id; }); 
 
-    var root = d3.hierarchy(hierarchy)
-        .sum(function(d) { return d.value; })
-        .each(function(d) {
-            d.id = d.data.name;
-        });
-
-    var node = svg.selectAll(".node")
-        .data(pack(root).leaves())
-        .enter().append("a")
-            .attr("class", "node")
-            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-            .attr("href", "#")
-    
-    node.append("circle")
-        .attr("id", function(d) { return d.name;})
-        .attr("r", function(d) { return d.r;})
-        .style("fill", function(d) { return color(d.value); })
-        .style("stroke", "black")
-        .style("stroke-width", "2px")
-
-    node.append("clipPath")
-        .attr("id", function(d) { return "clip-" + d.id; })
-        .append("use")
-        .attr("xlink:href", function(d) { return "#" + d.id; }); 
-
-    node.append("text")
-        .selectAll("tspan")
-        .data(function(d) { return d.id.split(/(?=[A-Z][^A-Z])/g); })
-        .enter().append("tspan")
-        .text(function(d) { return d })
-        .attr("x", 0)
-        .attr("y", 0)
-        .style("font-size", "1px")
-        .attr("class", "unselectable")
-        .each(getSize)
-        .style("font-size", function() { return this.scale + "px"; })
-        .attr("y", function(d, i, nodes){ return 0;})
-        //.attr("y", function(d, i, nodes) { return 20 + (i - nodes.length / 2 - 0.5) * 10; });
+        node.append("text")
+            .selectAll("tspan")
+                .data(function(d) {return d.id.split(/(?=[A-Z][^A-Z])/g); })
+                .enter().append("tspan")
+                    .text(function(d) { return d })
+                    .attr("x", 0)
+                    .attr("y", 0)
+                    .style("font-size", "1px")
+                    .attr("class", "unselectable")
+                    .each(getSize)
+                    .style("font-size", function() { return this.scale + "px"; })
+        }
 });
 
 function getSize(){
