da72f735b56c0f7f4e0854eb4d94b706e87348e7
Jeremie Roy <j-roy95@hotmail.com>
2017-08-16 23:01:45 -0400

    *Fix links on bubbles

--- public/javascripts/bubbles.js
+++ public/javascripts/bubbles.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
         });
         bubbles.push({
             "name": "Create bubble",
-            "createBubble": true,
+            "type": "createBubble",
             "value": 1
         });
         bubblesHierarchy.children = bubbles;
@@ -58,15 +58,9 @@ $(document).ready(function(){
 
         var node = svg.selectAll(".node")
             .data(pack(root).leaves())
-            .enter().append("a")
+            .enter().append("g")
                 .attr("class", "node")
                 .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-                .attr("href", function(d){
-                    if(d.data.createBubble === true){
-                        return undefined;
-                    }
-                    return d.id;
-                })
                 .attr("cursor", "pointer");
 
         node.append("circle")
@@ -81,10 +75,10 @@ $(document).ready(function(){
                 return color(d.value); 
             })
             .attr("class", function(d) {
-                if(d.data.createBubble === true){
-                    return "create-bubble";
+                if(d.data.type === undefined){
+                    return "default";
                 }
-                return undefined;
+                return d.data.type;
             })
             .style("stroke", "black")
             .style("stroke-width", "3px");
@@ -105,31 +99,27 @@ $(document).ready(function(){
                     .attr("class", "unselectable")
                     .each(getSize)
                     .style("font-size", function() { return this.scale + "px"; })
+                .on("click", function(text){
+                    if(text == "Create bubble"){
+                        openCreateBubbleDialog();
+                        return false;
+                    }
+                    window.location.href = "/" + text;
+                    return false;
+                })
+        
+        var defaultBubbles = d3.selectAll(".default")
+            .on("click", function(d) {
+                window.location.href = "/"+d.id;
+                return false;        
+            })
 
-        var createBubbleButton = d3.select(".create-bubble")
+        var createBubbleButton = d3.select(".createBubble")
             .attr("stroke-dasharray", "10,10")
             .attr("href", "#")
             .on("click", function(){
-                var test = $('<input class="form-control" placeholder="Enter a bubble name..."></input>');
-                BootstrapDialog.show({
-                    title: 'Create a bubble',
-                    id: "create-bubble-modal",
-                    message:  test,
-                    onshown: function(dialogRef){
-                        var input = $("input.form-control"); 
-                        input.focus();
-                        console.log(input);
-                        input.keypress(function(e){
-                            if(e.which == 13){
-                                window.location.href = "/"+input.val();
-                                return false;
-                            }
-                        });  
-                    }
-                });
+                openCreateBubbleDialog();
             });
-
-  
         }
 });
 
@@ -138,4 +128,22 @@ function getSize(){
     circleBoundingBox = this.parentNode.parentNode.getBBox(),
     scale = Math.min(circleBoundingBox.width / boundingBox.width, circleBoundingBox.height / boundingBox.height);
     this.scale = scale;
+}
+
+function openCreateBubbleDialog(){
+    BootstrapDialog.show({
+        title: 'Create a bubble',
+        id: "create-bubble-modal",
+        message: $('<input class="form-control" placeholder="Enter a bubble name..."></input>'),
+        onshown: function(dialogRef){
+            var input = $("input.form-control"); 
+            input.focus();
+            input.keypress(function(e){
+                if(e.which == 13){
+                    window.location.href = "/"+input.val();
+                    return false;
+                }
+            });  
+        }
+    });        
 }
